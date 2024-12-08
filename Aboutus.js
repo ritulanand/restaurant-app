a2b0d579f068696b04af427f4fae5a0496eb578f
Your Name <you@example.com>
2024-12-08 11:26:27 +0530

    qr code added

--- Aboutus.js
+++ Aboutus.js
@@ -1,19 +1,19 @@
 // script.js
 let currentIndex = 0;
 // Get the carousel track and items
-const carouselTrack = document.querySelector('.carousel-track');
-const testimonialItems = document.querySelectorAll('.testimonial-item');
+const carouselTrack = document.querySelector(".carousel-track");
+const testimonialItems = document.querySelectorAll(".testimonial-item");
 // Auto-scroll every 4 seconds
 function moveCarousel() {
-currentIndex++;
-if (currentIndex >= testimonialItems.length) {
-currentIndex = 0;
-}
-const offset =-currentIndex * 100; // Each item is 100% wide
-carouselTrack.style.transform = `translateX(${offset}%)`;
+  currentIndex++;
+  if (currentIndex >= testimonialItems.length) {
+    currentIndex = 0;
+  }
+  const offset = -currentIndex * 100; // Each item is 100% wide
+  carouselTrack.style.transform = `translateX(${offset}%)`;
 }
 function visitMenuHtml() {
-window.location.href = "ourmenu.html";
+  window.location.href = "ourmenu.html";
 }
 // Start the carousel
 setInterval(moveCarousel, 2000);
